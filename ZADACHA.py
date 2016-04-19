233e920147495ca38bcdaf09c199f91cd7482ce1
NaM-Cat <namcat@mail.ru>
2016-04-19 16:16:43 +0500

    Создали таблицу employ и доработал класс Papperboy для принятия на работу и внесения в таблицу бд

--- ZADACHA.py
+++ ZADACHA.py
@@ -15,9 +15,10 @@ class District:
         self.name = name
         self.population = population
         DISTRICTS.append(self)
+        print ('Добавляем район:', self.name)
         con = sqlite3.connect("catalog_zadach.db")
         cur = con.cursor()
-        cur.execute("INSERT INTO distr(name, population) VALUES (?,?)", (DISTRICTS[len(DISTRICTS)-1].name, DISTRICTS[len(DISTRICTS)-1].population))
+        cur.execute("INSERT INTO distr(name, population) VALUES (?,?)", (self.name, self.population))
         con.commit()
         cur.execute("SELECT * FROM distr")
         print (cur.fetchall())
@@ -33,6 +34,7 @@ class District:
     
     def change_population(self, new_population):
         self.population = new_population
+        print ('Изменяем численность района:', self.name)
         con = sqlite3.connect("catalog_zadach.db")
         cur = con.cursor()
         cur.execute("UPDATE distr SET population = (?) WHERE name = (?)", (new_population, self.name))
@@ -46,26 +48,46 @@ class District:
 class Employee:
     """ Класс всех сотрудников"""
     
-    def __init__(self, name, job = None):
+    def __init__(self, name, job):
         self.name = name
+        self.job = job
 
 class Papperboy(Employee):
     """ Разносчики газет"""
     
     def __init__(self, name, productivity = 0):
+        
         Employee.__init__(self, name, 'papperboy')
-        LIST_OF_RECRUTED.append(name, productivity, Employee.__init__.job)
         self.productivity = productivity
+        print ('Добавляем Разносчика газет:', self.name)
+        LIST_OF_RECRUTED.append(self)
     
+        con = sqlite3.connect("catalog_zadach.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO employ(name, job, productivity) VALUES (?,?,?)",
+                    (self.name,
+                     self.job,
+                     self.productivity))
+        con.commit()
+        cur.execute("SELECT * FROM employ")
+        print (cur.fetchall())
+        cur.close()
+        con.close()
+    
+    def __str__(self):
+        return '[Employee: %s, %s, %s]' % (self.name, self.job, self.productivity)
+    
+    def __conform__(self, protocol):
+        if protocol is sqlite3.PrepareProtocol:
+            return "%s|%s|%s" % (self.name, self.job, self.productivity)
+        
     def __del__(self):
         LIST_OF_FIRED.append(self)
         LIST_OF_RECRUTED.remove(self)
         
-        
+    def prdct_perm(x):
+        return x
    
-def prdct_perm():
-    return 2000
-
 def prdct_distr(district):
     return 1000*(district.population/1000)
 
@@ -74,25 +96,20 @@ def prdct_prb():
     prdct_list = [1200, 1000, 800]
     return rand_prb.choice(prdct_list, p=prb_list)
 
-
-
 con = sqlite3.connect("catalog_zadach.db")
 cur = con.cursor()
 try:
     cur.execute("DROP TABLE IF EXISTS distr")
     cur.execute("CREATE TABLE distr (id INTEGER PRIMARY KEY , name TEXT, population INTEGER)")
-#    for i in range(0,len(DISTRICTS)):
-#        cur.execute("INSERT INTO distr(name, population) VALUES (?,?)", (DISTRICTS[i].name, DISTRICTS[i].population))      
-    
-    #надо попробовать здесь прописать цикл вставок в таблицу для каждого дистрикта
-    #cur.executemany("INSERT INTO distr(name, population) VALUES (?,?)", (DISTRICTS[].name, DISTRICTS[].population))
+    cur.execute("DROP TABLE IF EXISTS employ")
+    cur.execute("CREATE TABLE employ (id INTEGER PRIMARY KEY , name TEXT, job TEXT, productivity INTEGER)")
 except sqlite3.DatabaseError:
     print ("Ошибка:")
 else:
     print ("Запрос успешно выполнен")
 con.commit()
-cur.execute("SELECT * FROM distr")
-print (cur.fetchall())
+cur.close()
+con.close()
 
 District('VIZ', 10000)
 District('Centr', 20000)
@@ -100,35 +117,21 @@ District('Uralmash', 15000)
 District('Elmash', 14000)
 District('Sortirovka', 7000)
 
-#DISTRICTS[0].population = 11000
-#print (DISTRICTS[0])
-
-#cur.execute("SELECT * FROM distr")
-#print (cur.fetchall())
-
-#cur.close()
-#con.close()
-#raw_input()
-
-#print (DISTRICTS)
-
 print('--------------------')
 
 DISTRICTS[0].change_population(11000)
 
-#Papperboy('Алексеев Геннадий Викторович')
-#Papperboy('Хазанов Владимир Андреевич')
-#Papperboy('Олейко Иван Петрович')
+print('--------------------')
 
-#print(LIST_OF_RECRUTED[1].name)
+Papperboy('Алексеев Геннадий Викторович',Papperboy.prdct_perm(2000))
+Papperboy('Хазанов Владимир Андреевич')
+Papperboy('Олейко Иван Петрович')
 
 print('--------------------')
 
 #LIST_OF_RECRUTED[0].productivity = prdct_perm
 #LIST_OF_RECRUTED[1].productivity = prdct_prb
 #LIST_OF_RECRUTED[2].productivity = prdct_distr(DISTRICTS[3])
-
-#print (LIST_OF_RECRUTED[2].productivity)
 
 print('--------------------')
 
