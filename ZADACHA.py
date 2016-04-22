75e188551fa51bdf1262416188cce61d9256f80e
NaM-Cat <namcat@mail.ru>
2016-04-23 01:02:07 +0500

    Начал писать функцию расчета дохода

--- ZADACHA.py
+++ ZADACHA.py
@@ -8,6 +8,11 @@ LIST_OF_RECRUTED = []   #Список принятых на работу
 LIST_OF_FIRED = []      #Список уволенных с работы
 DISTRICTS = []          #Список районов
 
+PRDCT_PERM = 2000               #количество зарабатываемое в день не зависимо от района города
+PRDCT_DISTR = 1000              #количество зарабатываемое в день на 1000 жителей в зависимости от района
+PRB_LIST = [0.17, 0.23, 0.6]    #вероятностное распределение на получение прибыли указанное в PRDCT_LIST
+PRDCT_LIST = [1200, 1000, 800]  #список количества зарабатываемого в день с 1000 жителей с распределение вероятностей PRB_LIST
+
 class District:
     """Районы города"""
     
@@ -20,10 +25,7 @@ class District:
         cur = con.cursor()
         cur.execute("INSERT INTO distr(name, population) VALUES (?,?)", (self.name, self.population))
         con.commit()
-        cur.execute("SELECT * FROM distr")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+        #print_table(cur,"SELECT * FROM distr")
         cur.close()
         con.close()
 
@@ -54,13 +56,18 @@ class Employee:
         self.job = job
 
 class Papperboy(Employee):
-    """ Разносчики газет"""
+    """ Разносчики газет
+        Параметр type_productivity определяет вид производительности труда разносчика газет:
+            1 - зарабатывает PRDCT_DISTR руб. в день на каждую 1000 жителей в районе
+            2 - забаратывает PRDCT_PERM руб. в независимости от района города
+            3 - зарабатывает сумму в PRDCT_LIST по вероятностному распределению PRB_LIST
+                на каждую 1000 жителей в районе"""
     
-    def __init__(self, name, productivity = 0):
+    def __init__(self, name, type_productivity):
         
         Employee.__init__(self, name, 'papperboy')
-        self.productivity = productivity
-        print ('Добавляем Разносчика газет:', self.name)
+        self.type_productivity = type_productivity
+        print ('Добавляем Разносчика газет:', self.name, self.job, self.type_productivity)
         LIST_OF_RECRUTED.append(self)
     
         con = sqlite3.connect("catalog_zadach.db")
@@ -68,18 +75,18 @@ class Papperboy(Employee):
         cur.execute("INSERT INTO employ(name, job, productivity) VALUES (?,?,?)",
                     (self.name,
                      self.job,
-                     self.productivity))
+                     self.type_productivity))
         con.commit()
-        print_table(cur,"SELECT * FROM employ")      
+        #print_table(cur,"SELECT * FROM employ")      
         cur.close()
         con.close()
     
     def __str__(self):
-        return '[Employee: %s, %s, %s]' % (self.name, self.job, self.productivity)
+        return '[Employee: %s, %s, %s]' % (self.name, self.job, self.type_productivity)
     
     def __conform__(self, protocol):
         if protocol is sqlite3.PrepareProtocol:
-            return "%s|%s|%s" % (self.name, self.job, self.productivity)
+            return "%s|%s|%s" % (self.name, self.job, self.type_productivity)
         
     def __del__(self):
         LIST_OF_FIRED.append(self)
@@ -94,13 +101,16 @@ def print_table(cursor, cmd):
     for row in rows:
         print(row)
 
-def prdct_distr(district):
-    return 1000*(district.population/1000)
-
-def prdct_prb():
-    prb_list = [0.17, 0.23, 0.6]
-    prdct_list = [1200, 1000, 800]
-    return rand_prb.choice(prdct_list, p=prb_list)
+def productivity(perm = 0, distr = None, prb = False):
+    if perm > 0 and distr == None and prb == False:
+        return perm
+    elif distr != None and perm == 0 and prb == False:
+        return PRDCT_DISTR*(distr.population/1000)
+    elif prb == True and perm == 0 and distr != None:
+        return rand_prb.choice(PRDCT_LIST, p=PRB_LIST)*(distr.population/1000)
+    else:
+        print ('Введены некорретные параметры')
+        return
 
 def index_max_popul_distr(list_of_distr):
     index = 0
@@ -114,8 +124,7 @@ def index_min_popul_distr(list_of_distr):
     for i in range(0,len(list_of_distr)-1):
         if(list_of_distr[i].population < list_of_distr[i+1].population):
             index = i
-    return index
-    
+    return index   
 
 def create_graphic(month, list_of_employ, list_of_distr):
     employ = 0
@@ -131,7 +140,7 @@ def create_graphic(month, list_of_employ, list_of_distr):
                         list_of_distr[distr].name))
             employ += 1
     con.commit()
-    print_table(cur,"SELECT * FROM graphic")
+ #   print_table(cur,"SELECT * FROM graphic")
     cur.close()
     con.close()
 
@@ -141,9 +150,15 @@ def change_graphic(date, employ_name, distr_name):
     cur.execute("UPDATE graphic SET employ_name = (?) WHERE distr_name = (?) and date = (?)",
                 (employ_name, distr_name, date))
     con.commit()
-    print_table(cur,"SELECT * FROM graphic")
+  #  print_table(cur,"SELECT * FROM graphic")
     cur.close()
     con.close()
+
+def calc_income(start_date, finish_date):
+    total_productivity = 0
+    for day in range(start_date.__getattribute__('day'), finish_date.__getattribute__('day')):
+        
+    
 
 con = sqlite3.connect("catalog_zadach.db")
 cur = con.cursor()
@@ -152,10 +167,10 @@ try:
     cur.execute("CREATE TABLE distr (id INTEGER PRIMARY KEY , name TEXT, population INTEGER)")
     cur.execute("DROP TABLE IF EXISTS employ")
     cur.execute("CREATE TABLE employ (id INTEGER PRIMARY KEY , name TEXT, job TEXT, productivity INTEGER)")
-    cur.execute("DROP TABLE IF EXISTS grafic")
+    cur.execute("DROP TABLE IF EXISTS graphic")
     cur.execute("CREATE TABLE graphic (date DATE, employ_name TEXT, distr_name TEXT)")
 except sqlite3.DatabaseError:
-    print ("Ошибка:")
+    print ("Ошибка в базе данных!!!!!!!!!!!!!!!")
 else:
     print ("Запрос успешно выполнен")
 con.commit()
@@ -174,9 +189,9 @@ DISTRICTS[0].change_population(11000)
 
 print('--------------------')
 
-Papperboy('Алексеев Геннадий Викторович',Papperboy.prdct_perm(2000))
-Papperboy('Хазанов Владимир Андреевич')
-Papperboy('Олейко Иван Петрович')
+Papperboy('Алексеев Геннадий Викторович',1)
+Papperboy('Хазанов Владимир Андреевич',2)
+Papperboy('Олейко Иван Петрович',3)
 
 print('--------------------')
 
@@ -192,10 +207,10 @@ print(Calendar.formatmonth(2016,4))
 print('--------------------')
 print(date(2016,4,11))
 try:
-    d=date(2016,2,30)
+    d=date(2016,2,28).__getattribute__('day')
 except ValueError or SyntaxError:
     pass
-#print(d)
+print(d)
 
 print('--------AAAAAAAAA------------')
 
@@ -213,6 +228,7 @@ cur.execute("SELECT * FROM graphic")
 rows = cur.fetchall()
 
 print(rows[0][2])
+print(productivity(prb = True))
 
 
 
