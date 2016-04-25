1ea71742a7eb3170da12f5fe10563f4384e2c72d
NaM-Cat <namcat@mail.ru>
2016-04-25 17:31:06 +0500

    1 Закончил функцию calc_income для одного месяца. 2 Внес дополнительную функцию для изменения типа производительности разносчика газет. 3 вставил везде (почти везде) комментарии 4 не доработал функцию __del__ в разносчике газет. работает не правильно пока

--- ZADACHA.py
+++ ZADACHA.py
@@ -17,6 +17,7 @@ class District:
     """Районы города"""
     
     def __init__(self, name, population):
+        
         self.name = name
         self.population = population
         DISTRICTS.append(self)
@@ -30,20 +31,26 @@ class District:
         con.close()
 
     def __str__(self):
+        
         return '[District: %s, %s]' % (self.name, self.population)
     
-   # def __conform__(self, protocol):
-   #     if protocol is sqlite3.PrepareProtocol:
-   #         return "%s|%s" % (self.name, self.population)
+    def __conform__(self, protocol):
+        
+        if protocol is sqlite3.PrepareProtocol:
+            return "%s|%s" % (self.name, self.population)
     
     def change_population(self, new_population):
+        """Функция замены численности района на значение new_population"""
+        
         self.population = new_population
+        
         print ('Изменяем численность района:', self.name)
+        
         con = sqlite3.connect("catalog_zadach.db")
         cur = con.cursor()
         cur.execute("UPDATE distr SET population = (?) WHERE name = (?)", (new_population, self.name))
         con.commit()
-        print_table(cur,"SELECT * FROM distr")
+        #print_table(cur,"SELECT * FROM distr")
         cur.close()
         con.close()
 
@@ -55,7 +62,7 @@ class Employee:
         self.name = name
         self.job = job
 
-class Papperboy(Employee):
+class Papperboy(Employee): 
     """ Разносчики газет
         Параметр type_productivity определяет вид производительности труда разносчика газет:
             1 - зарабатывает PRDCT_DISTR руб. в день на каждую 1000 жителей в районе
@@ -67,6 +74,7 @@ class Papperboy(Employee):
         
         Employee.__init__(self, name, 'papperboy')
         self.type_productivity = type_productivity
+        
         print ('Добавляем Разносчика газет:', self.name, self.job, self.type_productivity)
         LIST_OF_RECRUTED.append(self)
     
@@ -82,63 +90,72 @@ class Papperboy(Employee):
         con.close()
     
     def __str__(self):
+        
         return '[Employee: %s, %s, %s]' % (self.name, self.job, self.type_productivity)
     
     def __conform__(self, protocol):
+        
         if protocol is sqlite3.PrepareProtocol:
             return "%s|%s|%s" % (self.name, self.job, self.type_productivity)
         
-    def __del__(self):
+    def __del__(self):          
+        
         LIST_OF_FIRED.append(self)
         LIST_OF_RECRUTED.remove(self)
         
-    def prdct_perm(x):
-        return x
+        con = sqlite3.connect("catalog_zadach.db")
+        cur = con.cursor()
+        cur.execute("DELETE FROM employ WHERE name == (?) AND job == (?)",(self.name,self.job))
+        con.commit()
+        #print_table(cur,"SELECT * FROM employ")      
+        cur.close()
+        con.close()
+        
+    
+    def change_productivity(self, new_productivity):
+        """Функция изменения типа производительности разносчика газет на new_productivity"""
+        
+        self.type_productivity = new_productivity
+        
+        print ('Изменяем тип производительности разносчика газет:', self.name)
+        
+        con = sqlite3.connect("catalog_zadach.db")
+        cur = con.cursor()
+        cur.execute("UPDATE employ SET productivity = (?) WHERE name = (?)", (new_productivity, self.name))
+        con.commit()
+        print_table(cur,"SELECT * FROM employ")
+        cur.close()
+        con.close()
     
 def print_table(cursor, cmd):
+    """Функция вывода на печать результата ввода команды cmd для курсора cursor"""
+    
     cursor.execute(cmd)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-def productivity(perm = 0, distr = None, prb = False):
-    if perm > 0 and distr == None and prb == False:
-        return perm
-    elif distr != None and perm == 0 and prb == False:
-        return PRDCT_DISTR*(distr.population/1000)
-    elif prb == True and perm == 0 and distr != None:
-        return rand_prb.choice(PRDCT_LIST, p=PRB_LIST)*(distr.population/1000)
-    else:
-        print ('Введены некорретные параметры')
-        return
-
-def productivity_new(type_productivity, distr = None):
+def productivity(type_productivity, distr_population):
+    """Функция для расчета производительности труда для разносчика газет по заданным:
+    type_productivity - тип производительности труда разносчика газет
+    distr_population - численность района, в котором ведётся расчет производительности труда"""
+    
     if type_productivity == 1:
-        return PRDCT_DISTR*(distr.population/1000)
+        return PRDCT_DISTR*(distr_population/1000)
     elif type_productivity == 2:
         return PRDCT_PERM
     elif type_productivity == 3:
-        return rand_prb.choice(PRDCT_LIST, p=PRB_LIST)*(distr.population/1000)
+        return rand_prb.choice(PRDCT_LIST, p=PRB_LIST)*(distr_population/1000)
     else:
         print ('Введены некорретные параметры')
         return
 
-def index_max_popul_distr(list_of_distr):
-    index = 0
-    for i in range(0,len(list_of_distr)-1):
-        if(list_of_distr[i].population >= list_of_distr[i+1].population):
-            index = i
-    return index
-
-def index_min_popul_distr(list_of_distr):
-    index = 0
-    for i in range(0,len(list_of_distr)-1):
-        if(list_of_distr[i].population < list_of_distr[i+1].population):
-            index = i
-    return index   
-
 def create_graphic(month, list_of_employ, list_of_distr):
-    employ = 0
+    """Функция создания графика времени на месяц (month) и места работы (list_of_distr)
+    для имеющихся разносчиков газет(list_of_employ)"""
+    
+    employ = 0          #переменная для перебора всех разносчиков газет из list_of_employ
+        
     con = sqlite3.connect("catalog_zadach.db")
     cur = con.cursor()
     for day in range(1,calendar.monthrange(2016,month)[1]+1):
@@ -148,7 +165,7 @@ def create_graphic(month, list_of_employ, list_of_distr):
             cur.execute("INSERT INTO graphic(date, employ_name, distr_name) VALUES (?,?,?)",
                         (date(2016,month,day),
                         list_of_employ[employ].name,
-                        list_of_distr[distr]))
+                        list_of_distr[distr].name))
             employ += 1
     con.commit()
  #   print_table(cur,"SELECT * FROM graphic")
@@ -156,6 +173,8 @@ def create_graphic(month, list_of_employ, list_of_distr):
     con.close()
 
 def change_graphic(date, employ_name, distr_name):
+    """Функция изменения графика в дате date, сотрудника employ_name и/или района distr_name"""
+    
     con = sqlite3.connect("catalog_zadach.db")
     cur = con.cursor()
     cur.execute("UPDATE graphic SET employ_name = (?) WHERE distr_name = (?) and date = (?)",
@@ -166,19 +185,28 @@ def change_graphic(date, employ_name, distr_name):
     con.close()
 
 def calc_income(start_date, finish_date):
+    """Функция расчета предполагаемого дохода в месяце с start_date до finish_date"""
+    
     total_productivity = 0
     total_distr = len(DISTRICTS)
+    
     con = sqlite3.connect("catalog_zadach.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM graphic WHERE date > (?) and date < (?)",(start_date, finish_date))
+    cur.execute("SELECT graphic.date, employ.productivity, distr.population\
+                FROM graphic, employ, distr\
+                WHERE date > (?) AND date < (?)\
+                AND employ.name == graphic.employ_name\
+                AND distr.name == graphic.distr_name",(start_date, finish_date))
     rows = cur.fetchall()
- #   for row in rows:
- #       print(row)
-#    for day in range(start_date.__getattribute__('day'), finish_date.__getattribute__('day')):
-#        for i_disrt in range(0,total_distr):
-#            total_productivity += productivity_new(type_productivity, distr)
-
-   
+    #for row in rows:
+    #    print(row)
+    
+    for it in range(0,len(rows)):
+        total_productivity += productivity(rows[it][1], rows[it][2])
+    cur.close()
+    con.close()
+    
+    return total_productivity
 
 con = sqlite3.connect("catalog_zadach.db")
 cur = con.cursor()
@@ -212,12 +240,15 @@ print('--------------------')
 Papperboy('Алексеев Геннадий Викторович',1)
 Papperboy('Хазанов Владимир Андреевич',2)
 Papperboy('Олейко Иван Петрович',3)
+Papperboy('Кременко Алексей Викторович',3)
 
 print('--------------------')
 
-#LIST_OF_RECRUTED[0].productivity = prdct_perm
-#LIST_OF_RECRUTED[1].productivity = prdct_prb
-#LIST_OF_RECRUTED[2].productivity = prdct_distr(DISTRICTS[3])
+LIST_OF_RECRUTED[0].change_productivity(3)
+
+print('--------------------')
+
+LIST_OF_RECRUTED.pop(3)
 
 print('--------------------')
 
@@ -225,31 +256,16 @@ Calendar = calendar.TextCalendar()
 print(Calendar.formatmonth(2016,4))
 
 print('--------------------')
-print(date(2016,4,11))
-try:
-    d=date(2016,2,28).__getattribute__('day')
-except ValueError or SyntaxError:
-    pass
-print(d)
-
-print('--------AAAAAAAAA------------')
 
 create_graphic(4,LIST_OF_RECRUTED,DISTRICTS)
 
-print('--------AAAAAAAAA------------')
+print('--------------------')
 
 change_graphic(date(2016,4,1),'Алексеев Геннадий Викторович','Centr')
 
-print('--------AAAAAAAAA------------')
+print('--------------------')
 
-con = sqlite3.connect("catalog_zadach.db")
-cur = con.cursor()
-cur.execute("SELECT * FROM graphic WHERE date > '2016-04-20' and date < '2016-04-26'")
-rows = cur.fetchall()
-print_table(cur,"SELECT * FROM graphic WHERE date > '2016-04-20' and date < '2016-04-26'")
-print(rows[0][2].name)
-print(len(DISTRICTS))
-calc_income('2016-04-10','2016-04-21') 
+print(calc_income('2016-04-10','2016-04-21'))
 
 
 
